ab93915b736359f2afad5efc12fb047d5de7f072
Troy Harris <troy.harris@phxschools.org>
2017-03-01 10:06:12 -0700

    Initial integration of pyodbc. Created a testdb script

---
+++ akbash/connectors/testdb.py
@@ -0,0 +1,18 @@
+import pyodbc
+import dbauth
+
+# Vars assigned in dbauth.py which is ignored in the repo
+login = dbauth.visions["login"]
+password = dbauth.visions["password"]
+server = dbauth.visions["server"]
+database = dbauth.visions["database"]
+
+connection = pyodbc.connect('DRIVER=FreeTDS;SERVER=%s;PORT=1433;DATABASE=%s;UID=%s;PWD=%s' % (server, database, login, password))
+
+try:
+    cursor = connection.cursor()
+except e:
+    if e.__class__ == pyodbc.ProgrammingError:
+        print("DB Error")
+
+print("DB Okay")
