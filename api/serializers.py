73c2b3493d90874225cfa73ab34a2ebd33de8853
Troy Harris <troy.harris@phxschools.org>
2017-06-06 13:25:44 -0700

    Created a WorkflowComplete API view

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from api.models import Employee, Service, Contractor, Vendor, Position, Location, Department, PositionType
+from api.models import Employee, Service, Contractor, Vendor, Position, Location, Department, PositionType, Person
 
 
 # As of now, Services are never directly exposed through REST
@@ -30,6 +30,71 @@ class VendorSerializer(serializers.ModelSerializer):
 
 
 # The following serializers are exposed in the API
+class PersonSerializer(serializers.ModelSerializer):
+    # Expose the URL to access employee-detail
+    api_url = serializers.HyperlinkedIdentityField(view_name='employee-detail', format='html')
+    # Expose the usernames of various workflow update fields
+    onboarded_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    tcp_fingerprinted_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    badge_created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    emp_record_created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    position_linked_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    visions_account_created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    synergy_account_created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+    ad_account_created_by = serializers.SlugRelatedField(many=False, read_only=True, slug_field='username')
+
+    # Will pull in any Service usernames (Visions, Synergy, etc)
+    services = ServiceSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Person
+        fields = (
+            "api_url",
+            "id",
+            "first_name",
+            "last_name",
+            "middle_name",
+            "badge_number",
+            "birth_date",
+            "gender",
+            "race_white",
+            "race_asian",
+            "race_black",
+            "race_islander",
+            "race_american_indian",
+            "ethnicity",
+            "hqt",
+            "ssn",
+            "tcp_id",
+            "talented_id",
+            "is_onboarded",
+            "onboarded_date",
+            "onboarded_by",
+            "is_tcp_fingerprinted",
+            "tcp_fingerprinted_date",
+            "tcp_fingerprinted_by",
+            "is_badge_created",
+            "badge_created_date",
+            "badge_created_by",
+            "is_emp_record_created",
+            "emp_record_created_date",
+            "emp_record_created_by",
+            "is_position_linked",
+            "position_linked_date",
+            "position_linked_by",
+            "is_visions_account_created",
+            "visions_account_created_date",
+            "visions_account_created_by",
+            "is_synergy_account_created",
+            "synergy_account_created_date",
+            "synergy_account_created_by",
+            "is_ad_account_created",
+            "ad_account_created_date",
+            "ad_account_created_by",
+            "services",
+        )
+
+
 class EmployeeSerializer(serializers.ModelSerializer):
     # Expose the URL to access employee-detail
     api_url = serializers.HyperlinkedIdentityField(view_name='employee-detail', format='html')
