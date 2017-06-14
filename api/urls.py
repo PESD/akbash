da824240f3856d4cc26bc1ecbdc4a82547452890
Troy Harris <troy.harris@phxschools.org>
2017-06-14 09:01:11 -0700

    Fixed URL problem with api app and employees end-point

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,13 @@ from rest_framework.routers import DefaultRouter
 
 # Django REST Framework routers
 router = DefaultRouter()
-router.register(r'employee', views.EmployeeViewSet)
+router.register(r'employee', views.EmployeeViewSet, 'employee')
 router.register(r'contractor', views.ContractorViewSet)
 router.register(r'position', views.PositionViewSet)
 router.register(r'location', views.LocationViewSet)
 router.register(r'department', views.DepartmentViewSet)
 router.register(r'position-type', views.PositionTypeViewSet)
-router.register(r'employee-no-workflow', views.EmployeeNoWorkflowViewSet)
+router.register(r'employee-no-workflow', views.EmployeeNoWorkflowViewSet, 'employee-no-workflow')
 router.register(r'person', views.PersonViewSet)
 
 urlpatterns = [
