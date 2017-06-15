cf99895f30e7f3d87ed8e8bdd8b0a362ddf1f5aa
Robert Towner <rob.towner@phxschools.org>
2017-06-15 16:18:50 -0700

    This stuff is not working out.

--- akjob/akjob_daemon.py
+++ akjob/akjob_daemon.py
@@ -8,24 +8,67 @@ default that python-daemon uses good enough?
 Start the daemon using AppConfig.ready()
 """
 
-import daemon
-import pid
+# import pid
+from daemon import runner, DaemonContext
 from time import sleep
 
 
-# def do_the_things():
-#     pass
+class DaemonRunner(runner.DaemonRunner):
+    "Overriding non-compatable python 2 code."
+
+    def __init__(self, app):
+        """ Set up the parameters of a new runner.
+
+            :param app: The application instance; see below.
+            :return: ``None``.
+
+            The `app` argument must have the following attributes:
+
+            * `stdin_path`, `stdout_path`, `stderr_path`: Filesystem paths
+              to open and replace the existing `sys.stdin`, `sys.stdout`,
+              `sys.stderr`.
+
+            * `pidfile_path`: Absolute filesystem path to a file that will
+              be used as the PID file for the daemon. If ``None``, no PID
+              file will be used.
+
+            * `pidfile_timeout`: Used as the default acquisition timeout
+              value supplied to the runner's PID lock file.
+
+            * `run`: Callable that will be invoked when the daemon is
+              started.
+
+            """
+        self.parse_args()
+        self.app = app
+        self.daemon_context = DaemonContext()
+        self.daemon_context.stdin = open(app.stdin_path, 'rt')
+        self.daemon_context.stdout = open(app.stdout_path, 'w+t')
+        self.daemon_context.stderr = open(app.stderr_path, 'w+t')
+
+        self.pidfile = None
+        if app.pidfile_path is not None:
+            self.pidfile = runner.make_pidlockfile(
+                app.pidfile_path, app.pidfile_timeout)
+        self.daemon_context.pidfile = self.pidfile
 
 
-# def start_daemon():
-with daemon.DaemonContext(pidfile=pid.PidFile('/Users/robwirk/dev/akbash/akjob.pid')):
-    # with daemon.DaemonContext(): # This works. The above with pidfile doesn't
-    # work.
-    while True:
-        # do_the_things()
-        sleep(6)
+# with daemon.DaemonContext(pidfile=pid.PidFile('/Users/robwirk/dev/akbash/akjob.pid')):
+class App():
+
+    def __init__(self):
+        self.pidfile_path = '/Users/robwirk/dev/akbash/akjob.pid'
+        self.stdin_path = '/dev/null'
+        self.stdout_path = '/dev/tty'
+        self.stderr_path = '/dev/tty'
+        self.pidfile_timeout = 5
+
+    def run(self):
+        while True:
+            # do_the_things()
+            sleep(10)
 
 
-# Enable to daemon to be started from the command line.
-# if __name__ == '__main__':
-#     start_daemon()
+app = App()
+daemon_runner = DaemonRunner(app)
+daemon_runner.do_action()
