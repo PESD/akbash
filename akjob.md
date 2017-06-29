a7a6a9621b1975aca5b5ec4e43acd0a93c3c74fe
Robert Towner <rob.towner@phxschools.org>
2017-06-29 13:39:43 -0700

    More work on Job model.

--- akjob.md
+++ akjob.md
@@ -1,2 +1,8 @@
 # Daemon
 The daemon starts when Akbash starts. There is no monitoring to restart the daemon if it crashes or otherwise stop. A cronjob could be created to attempt to start the daemon periodically.
+# akjob.models.Job
+Pendulum: https://pendulum.eustace.io/
+Shortcuts to Pendulum:
+* Job.interval
+* Job.datetime
+* Job.now
--- akjob/models.py
+++ akjob/models.py
@@ -1,77 +1,168 @@
+import pendulum
 from django.db import models
+
+""" I thought I needed these for field "choices" options. Don't need them.
+MONTHS = (
+    (1, "January"),
+    (2, "February"),
+    (3, "March"),
+    (4, "April"),
+    (5, "May"),
+    (6, "June"),
+    (7, "July"),
+    (8, "August"),
+    (9, "September"),
+    (10, "October"),
+    (11, "November"),
+    (12, "December")
+)
+
+DAYS = (
+    (0, "Sunday"),
+    (1, "Monday"),
+    (2, "Tuesday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+    (6, "Saturday"),
+)
+"""
 
 class Job(models.Model):
     "Job definitions"
 
-    MONTHS = (
-        (1, "January"),
-        (2, "February"),
-        (3, "March"),
-        (4, "April"),
-        (5, "May"),
-        (6, "June"),
-        (7, "July"),
-        (8, "August"),
-        (9, "September"),
-        (10, "October"),
-        (11, "November"),
-        (12, "December")
-    )
 
-    DAYS = (
-        (0, "Sunday"),
-        (1, "Monday"),
-        (2, "Tuesday"),
-        (3, "Wednesday"),
-        (4, "Thursday"),
-        (5, "Friday"),
-        (6, "Saturday"),
-    )
+    name = models.CharField(
+        max_length=32,
+        null=False,
+        blank=False,
+        default=None,  # Used to cause error if name isn't provided.
+        unique=False,
+        help_text="Name the job. Repeat names are allowed. Required Field.")
+
 
     """ Schedule One off job """
     run_once_at = models.DateTimeField(
+        null=True,
         blank=True,
-        help_text="Date and time to run the job. Used for one off job runs.")
+        help_text="Date and time to run the job.")
 
     """ Schedule reoccuring jobs """
+    # TODO: make a validation to only allow positive duration.
     run_every = models.DurationField(
+        null=True,
         blank=True,
         help_text="Run every ___. Format: [DD] [HH:[MM:]]ss. Schedule the job" +
                   "to repeatedly run after a time interval. Ex., Run every" +
                   "05:00 (5 minutes)")
     run_monthly = models.CharField(
+        max_length=124,
+        null=True,
         blank=True,
         help_text="Comma seperated list of days of the month to run the job." +
                   " Ex., 1,15 means run on the 1st and 15th of each month.")
-    run_weekly = models.CharField(blank=True)  # Day of week
-    run_monthly_time = models.TimeField(blank=True)
-    run_weekly_time = models.TimeField(blank=True)
-    # Run this many times. Count down each run.
-    run_count_limit = models.IntegerField(blank=True)
+    run_monthly_time = models.TimeField(
+        null=True,
+        blank=True,
+        help_text="Time of day to run the monthly / days of month jobs.")
+    run_weekly = models.CharField(
+        max_length=21,
+        null=True,
+        blank=True,
+        help_text="Comma seperated list of days of the week to run the job. " +
+                  "Use integers for each day starting at 0 for Sunday, 1 for " +
+                  "Monday, and so on to 6 for Saturday.")
+    run_weekly_time = models.TimeField(
+        null=True,
+        blank=True,
+        help_text="Time of day to run the weekly / days of week jobs.")
+    run_count_limit = models.IntegerField(
+        null=True,
+        blank=True,
+        help_text="Each time the job is run, the run count is incremented by " +
+                  "1. You may set a limit to how many times the job runs. " +
+                  "When the run count exceeds this value, the job is no " +
+                  "longer scheduled to run.")
 
     """ Limit job run to a window of time. """
-    active_time_begin = models.TimeField(blank=True)
-    active_time_end = models.TimeField(blank=True)
-    active_days = models.CharField(blank=True, choices=DAYS)  # List of days of week
-    active_months = models.Integer(blank=True, choices=MONTHS)  # List of Months
-    active_date_begin = models.DateField(blank=True)
-    active_date_end = models.DateField(blank=True)
+    active_time_begin = models.TimeField(
+        null=True,
+        blank=True,
+        help_text="Limit job runs to a time of day window between " +
+                  "active_time_begin and active_time_end.")
+    active_time_end = models.TimeField(
+        null=True,
+        blank=True,
+        help_text="Limit job runs to a time of day window between " +
+                  "active_time_begin and active_time_end.")
+    active_dow = models.CharField(
+        max_length=21,
+        null=True,
+        blank=True,
+        help_text="Limit job runs to the listed days of week. " +
+                  "Comma seperated list of days of the week. " +
+                  "Use integers for each day starting at 0 for Sunday, 1 for" +
+                  "Monday, and so on to 6 for Saturday.")
+    active_days = models.CharField(
+        max_length=124,
+        null=True,
+        blank=True,
+        help_text="Limit job runs to the listed days. Comma seperated list of " +
+                  "days of the month to limit the job run times to. Ex., 1,15 " +
+                  "means limit the run to the 1st and 15th of each month.")
+    active_months = models.CharField(
+        max_length=48,
+        null=True,
+        blank=True,
+        help_text="Limit job runs to the listed months. Comma seperated list " +
+                  "of integer months to limit the job run times to. " +
+                  "1 for January, 2 for February, 12 for December.")
+    active_date_begin = models.DateField(
+        null=True,
+        blank=True,
+        help_text="Limit job runs times to the dates between " +
+                  "active_date_begin and active_date_end.")
+    active_date_end = models.DateField(
+        null=True,
+        blank=True,
+        help_text="Limit job runs times to the dates between " +
+                  "active_date_begin and active_date_end.")
 
 
     """ Job run status """
     # Datetime of the next run. Blank if no future runs scheduled.
-    next_run = models.DateTimeField(blank=True)
+    next_run = models.DateTimeField(null=True, blank=True)
     # Datetime of last run. Also shows that the last run completed.
     # create logic that if current time >= next_run and last_run < next run,
     # then the job needs to be run now.
-    last_run = models.DateTimeField(blank=True)
+    last_run = models.DateTimeField(null=True, blank=True)
     # Use job_running to make sure not to run the job if it's currently
     # running.  # Job currently running, true/false.
     job_running = models.BooleanField(default=False)
     # how many times the job has been run
-    run_count = models.Integer(default=0)
+    run_count = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        if self.id and self.name:
+            return str(self.id) + " - " + self.name
+        elif self.id:
+            return str(self.id)
+        elif self.name:
+            return "No id - " + self.name
+        else:
+            return "No id - Unsaved Job"
+
+
+    """ Shortcuts to pendulum """
+    interval = pendulum.interval
+    datetime = pendulum.Pendulum
+    dtcreate = pendulum.create
+    now = pendulum.now
+    timezone = pendulum.timezone
 
 
 
-class Akjobs(models.Model):
-    pass
+
+# class Akjobs(models.Model):
+#     pass
